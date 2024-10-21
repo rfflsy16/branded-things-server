1ead1cb9edf8f8bd5420d386086b300adeb052e8
Raffles <rfflsy16@gmail.com>
2024-10-21 14:14:23 +0700

    feat finish gc01

--- bin/www.js
+++ bin/www.js
@@ -2,5 +2,5 @@ const app = require('../app')
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
+    console.log(`mangan ngap harganya cuman ${port}`)
 })
